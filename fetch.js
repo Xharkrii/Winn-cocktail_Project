bddc13eca21b5e9800c805c22236c1348d1bb29f
Xharkrii <eeke242004@gmail.com>
2026-07-08 04:04:13 -0700

    Initial commit

---
+++ fetch.js
@@ -0,0 +1,23 @@
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const searchParams = ''
+
+function fetchCocktailList(){
+    fetch(`${url}${searchParams}`)
+    .then(
+        (resp)=>{
+            if (resp.status === 200) {
+                return resp.json()
+            }
+            else{
+                return
+            }
+        }
+    )
+    .then(
+       (data)=>{
+        const drinksArray = data
+       } 
+    )
+}
+
+fetchCocktailList()
