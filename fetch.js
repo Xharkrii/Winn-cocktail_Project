018cf79a76bda2222db626b6d11cbe3ae0bef2af
Xharkrii <eeke242004@gmail.com>
2026-07-13 03:56:34 -0700

    Major change commit

--- fetch.js
+++ fetch.js
@@ -3,9 +3,16 @@ const container = document.querySelector('.cocktail-list')
 const input = document.getElementById('input')
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const searchParams = 'apple'
+let searchParams = ''
 
 function fetchCocktailList(){
+    const inputValue = input.value
+    if (inputValue === '') {
+        input = 'a'
+    }
+    else{
+        searchParams = inputValue
+    }
     fetch(`${url}${searchParams}`)
     .then(
         (resp)=>{
@@ -21,6 +28,31 @@ function fetchCocktailList(){
        (data)=>{
         const drinksArray = data.drinks
         console.log(drinksArray);
+
+        container.innerHTML = ''
+        drinksArray.forEach(singleDrink => {
+            container.innerHTML += `
+                <article>
+                    <div class="image">
+                        <img src=${singleDrink.strDrinkThumb} alt="">
+                    </div>
+                    <div class="description">
+                        <h4>${singleDrink.strDrink}</h4>
+                        <div class="sub">
+                            <p>Category:</p>
+                            <p>${singleDrink.strCategory}</p>
+                        </div>
+                        <div class="sub">
+                            <p>Serving Glass:</p>
+                            <p>${singleDrink.strGlass}</p>
+                        </div>
+                        <a href="">
+                            <button>More Details</button>
+                        </a>
+                    </div>  
+                </article>
+            `
+        })
        } 
     )
 }
